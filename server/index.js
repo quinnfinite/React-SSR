77287e9ee7771bc578feb8bd97fa1c3cd8d86be8
quinnfinite <quinn.royston@gmail.com>
2020-09-08 14:57:42 -0700

    can now use import inside of server/index.js via @bable/node & babelrc file

--- server/index.js
+++ server/index.js
@@ -1,12 +1,17 @@
+import React from 'react';
+
 const express = require('express');
 const app = express();
-const path = require('path')
+const path = require('path');
+
 
 app.use(express.static(path.join(__dirname, '../public')))
 
 const port = 8000;
 
 app.get('/', (req, res) => {
+
+
   res.sendFile('index');
 })
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,11 @@
 const path = require('path');
-const webpack = require('webpack')
+const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
   entry: './client/index.js',
   mode: 'development',
+  target: 'node',
+  externals: [nodeExternals()],
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'public/dist'),
@@ -15,9 +17,10 @@ module.exports = {
       use: {
         loader: 'babel-loader',
         options: {
-          presets:['@babel/preset-env', '@babel/preset-react']
+          presets:[['@babel/preset-env', {"targets": {"node": "current"}}], '@babel/preset-react']
         }
       }
     }]
-  }
+  },
+  plugins: [["@babel/plugin-proposal-class-properties"]]
 }
