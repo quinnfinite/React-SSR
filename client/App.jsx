5ea991a46dd15e235a2e8c68ab664b7f4d576fb6
quinnfinite <quinn.royston@gmail.com>
2020-09-08 14:16:37 -0700

    setup basic state mgmt w/hooks

--- client/App.jsx
+++ client/App.jsx
@@ -1,16 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ReactDom from 'react-dom';
 
-class App extends React.Component {
-  constructor(){
-    super()
+// class App extends React.Component {
+//   constructor(){
+//     super()
 
-  }
-  render(){
-    return (
-      <h1>App Component</h1>
-    )
-  }
+//   }
+//   render(){
+//     return (
+//       <h1>App Component</h1>
+//     )
+//   }
+// }
+
+function App() {
+  const [count, setCount] = useState(0)
+  return (
+    <>
+      <h1>Count: {count}</h1>
+      <button onClick={() => setCount(count+1)}>Increment</button>
+      <button onClick={() => setCount(count-1)}>Decrement</button>
+    </>
+  )
 }
 
 ReactDom.render(<App/>, document.getElementById('app'))
